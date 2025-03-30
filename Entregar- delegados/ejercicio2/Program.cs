c6120863b4cd162f811d20489cf82625a2cc3a3a
Dianaestmch <145007533+Dianaestmch@users.noreply.github.com>
2025-03-30 14:22:41 +0200

    Add files via upload

---
+++ Entregar- delegados/ejercicio2/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    public delegate void Infinitivo();
+    public static void Ser()
+    {
+        Console.WriteLine("BE");
+    }
+
+    public static void Correr()
+    {
+        Console.WriteLine("RUN");
+    }
+
+    public static void Ver()
+    {
+        Console.WriteLine("SEE");
+    }
+
+    public static void Pensar()
+    {
+        Console.WriteLine("Think");
+    }
+
+    public static void Comer()
+    {
+        Console.WriteLine("Eat");
+    }
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        Infinitivo verbos = null;
+        //verbos.Invoke();
+        verbos+= Ser;
+        verbos+= Correr;
+        verbos+= Ver;
+        
+        Console.WriteLine("Métodos añadidos");
+        verbos.Invoke();
+
+        verbos -= Ser;
+        verbos -= Ver;
+        verbos += Pensar;
+        verbos += Comer;
+
+        Console.WriteLine("Métodos eliminados");
+        verbos.Invoke();
+        
+        //Console.WriteLine(verbos.ToString());
+    }
+}
---
+++ Entregar- delegados/ejercicio3/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    public static void Mostrar<T>(T[] matriz)
+    {
+        foreach (var item in matriz)
+        {
+            Console.Write(item + "\t");
+        }
+    }
+
+    public delegate void MuestraDelegado<T>(T[] matriz);
+
+    private static void Main(string[] args)
+    {
+       // Muestra<int> muestra = null;
+        //Console.WriteLine("Hello, World!");
+        int[] numeros = { 1, 2, 3, 4 };
+        Mostrar(numeros);
+        Console.WriteLine("");
+        string[] palabras = {"SAL", "AGUA", "AZUCAR", "VINO" };
+        Mostrar(palabras);
+
+        Console.WriteLine("");
+
+        Console.WriteLine("Ahora con delegate");
+        MuestraDelegado <int> delegadoInt = Mostrar;
+        MuestraDelegado <string> delegadoString = Mostrar;
+
+        delegadoInt(numeros);
+        delegadoString(palabras);
+
+    }
+}
